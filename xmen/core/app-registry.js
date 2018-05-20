9a8f6e4592c0613b8fedbf7b2bd4fc5f2e41106f
John Taylor <johntayl89@gmail.com>
2018-05-20 13:53:12 -0400

    Loading Token model.

--- xmen/core/app-registry.js
+++ xmen/core/app-registry.js
@@ -1,103 +1,107 @@
 /**
- * XMEN Core apps registry module for loading installed apps. 
+ * XMEN Core apps registry module for loading installed apps.
  */
 
-const fs = require('fs');
-const express = require('express');
-const mongoose = require('mongoose');
+const fs = require("fs");
+const express = require("express");
+const mongoose = require("mongoose");
 
-const { AppHasBeenRegistered, AppFailedToRegister } = require('./exceptions');
-
+const { AppHasBeenRegistered, AppFailedToRegister } = require("./exceptions");
 
 class AppRegistryModule {
+  constructor() {
+    this.registeredApps = [];
+    this.appViews = [];
+  }
 
-    constructor() {
-        this.registeredApps = [];
-        this.appViews = [];
+  /**
+   * Register an app and throw an execption if already regsitered.
+   * @param {*} app
+   */
+  registerApp(app) {
+    // Make sure app hasn't been registered already
+    if (this.registeredApps.indexOf(app) > -1) {
+      throw new AppHasBeenRegistered(
+        `App '${app}' has already been registered`
+      );
+      return;
     }
 
+    // Load all models in app.
+    this.loadModels(app);
 
-    /**
-     * Register an app and throw an execption if already regsitered.
-     * @param {*} app 
-     */
-    registerApp(app) {
-        // Make sure app hasn't been registered already
-        if (this.registeredApps.indexOf(app) > -1) {
-            throw new AppHasBeenRegistered(`App '${app}' has already been registered`);
-            return;
-        }
+    // Load all routes in app.
+    this.loadRoutes(app);
 
-        // Load all models in app.
-        this.loadModels(app);
+    this.registeredApps.push(app);
+  }
 
-        // Load all routes in app.
-        this.loadRoutes(app);
+  /**
+   * Register all the apps provided.
+   * @param {*} apps
+   */
+  registerApps(apps) {
+    apps.map(app => this.registerApp(app));
 
-        this.registeredApps.push(app);
-    }
+    // Set the views.
+    xmen.app.set("views", xmen.config.appRoot);
+  }
 
+  /**
+   * If a models path exists in the app, load all models.
+   * @param {*} app
+   */
+  loadModels(app) {
+    let modelsPath = `${xmen.config.appRoot}/${app}/models`;
+    const ContentType = mongoose.model("ContentType");
+    const Permission = mongoose.model("Permission");
+    try {
+      if (fs.existsSync(modelsPath)) {
+        fs.readdirSync(modelsPath).forEach(file => {
+          if (file) {
+            let modelFile = `${modelsPath}/${file}`;
+            if (fs.existsSync(modelFile)) {
+              let modelSchema = require(modelFile);
 
-    /**
-     * Register all the apps provided.
-     * @param {*} apps 
-     */
-    registerApps(apps) {
-        apps.map(app => this.registerApp(app));
-
-        // Set the views.
-        xmen.app.set('views', xmen.config.appRoot);
-    }
-
-
-    /**
-     * If a models path exists in the app, load all models.
-     * @param {*} app 
-     */
-    loadModels(app) {
-        let modelsPath = `${xmen.config.appRoot}/${app}/models`;
-        const ContentType = mongoose.model('ContentType');
-        const Permission = mongoose.model('Permission');
-        try {
-            if (fs.existsSync(modelsPath)) {
-                fs.readdirSync(modelsPath).forEach((file) => {
-                    if (file) {
-                        let modelFile = `${modelsPath}/${file}`;
-                        if (fs.existsSync(modelFile)) {
-                            let modelSchema = require(modelFile);
-
-                            ContentType.addModel(app, modelSchema.collection.name, (err, model) => {
-                                // Permissions create from model.
-                                Permission.createModelPermissions(model, modelSchema.collection.name)
-                            });
-                        }
-                    }
-                });
+              ContentType.addModel(
+                app,
+                modelSchema.collection.name,
+                (err, model) => {
+                  // Permissions create from model.
+                  Permission.createModelPermissions(
+                    model,
+                    modelSchema.collection.name
+                  );
+                }
+              );
             }
-        } catch (e) {
-            throw new AppFailedToRegister(`App ${app} failed to load models.`, {
-                modelsPath: `${xmen.config.appRoot}/${app}/models`,
-                app: app,       
-            });
-        }
+          }
+        });
+      }
+    } catch (e) {
+      throw new AppFailedToRegister(`App ${app} failed to load models.`, {
+        modelsPath: `${xmen.config.appRoot}/${app}/models`,
+        app: app
+      });
     }
+  }
 
-
-    /**
-     * If a routes file exists in the app, load the routes.
-     * @param {*} app 
-     */
-    loadRoutes(app) {
-        let routesFile = `${xmen.config.appRoot}/${app}/routes`;
-        try {
-            require(routesFile)(xmen.app);
-        } catch(e) {
-            throw new AppFailedToRegister(`App ${app} failed to load routes.`, {
-                app: app,
-                routesFile: routesFile
-            });
-        }
+  /**
+   * If a routes file exists in the app, load the routes.
+   * @param {*} app
+   */
+  loadRoutes(app) {
+    let routesFile = `${xmen.config.appRoot}/${app}/routes`;
+    try {
+      require(routesFile)(xmen.app);
+    } catch (e) {
+      console.log(e.message);
+      throw new AppFailedToRegister(`App ${app} failed to load routes.`, {
+        app: app,
+        routesFile: routesFile
+      });
     }
+  }
 }
 
 module.exports.AppRegistry = new AppRegistryModule();
--- xmen/xmen.js
+++ xmen/xmen.js
@@ -1,223 +1,227 @@
 /**
  * XMEN Server application
- * 
+ *
  * Bootstrap application, create database connection, mount express routes,
  * require mongoose models.
  */
 
 "use strict";
 
-const express = require('express');
-const http = require('http');
-const errorhandler = require('errorhandler');
-const passport = require('passport');
-const mongoose = require('mongoose');
-const compression = require('compression');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const methodOverride = require('method-override');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const http = require("http");
+const errorhandler = require("errorhandler");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const compression = require("compression");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const helmet = require("helmet");
+const cors = require("cors");
 
-const logger = require('./debug/logger');
-let { AppRegistry } = require('./core/app-registry');
-
+const logger = require("./debug/logger");
+let { AppRegistry } = require("./core/app-registry");
 
 /**
  * Xmen server.
  */
 class Xmen {
-    constructor() {
-        // Express app
-        this.app = express();
+  constructor() {
+    // Express app
+    this.app = express();
 
-        // Server.
-        this.server = null;
+    // Server.
+    this.server = null;
 
-        // Logger
-        this.log = logger;
+    // Logger
+    this.log = logger;
 
-        this.config = {
-            startTime: Date.now(),
-            root: process.cwd(),
-            host: process.env.HOST || 'localhost',
-            port: process.env.PORT || 8080,
-            environment: process.env.NODE_ENV || 'development',
-            database: null,
-            appRoot: process.cwd() + "/app",
-            staticRoot: process.cwd() + "/public",
-            cacheView: false,
-            installedApps: [],
-        };
+    this.config = {
+      startTime: Date.now(),
+      root: process.cwd(),
+      host: process.env.HOST || "localhost",
+      port: process.env.PORT || 8080,
+      environment: process.env.NODE_ENV || "development",
+      database: null,
+      appRoot: process.cwd() + "/app",
+      staticRoot: process.cwd() + "/public",
+      cacheView: false,
+      installedApps: []
+    };
+  }
+
+  start(config = {}, callback) {
+    try {
+      this.log.debug(
+        `Server Setup: Loading config for ${this.config.environment}`
+      );
+      this.config = Object.assign(this.config, config[this.config.environment]);
+
+      this.bootstrap();
+    } catch (e) {
+      this.log.debug(`XMEN server could not start correctly.`);
+      this.log.error(e);
+      this.log.error(e.stack);
     }
+  }
 
-    start(config = {}, callback) {
-        try {
-            this.log.debug(`Server Setup: Loading config for ${this.config.environment}`);
-            this.config = Object.assign(this.config, config[this.config.environment]);
+  bootstrap() {
+    // Connect to database.
+    this.database();
 
-            this.bootstrap();
-        } catch (e) {
-            this.log.debug(`XMEN server could not start correctly.`);
-            this.log.error(e);
-            this.log.error(e.stack);
-        }
-    }
+    // Set up middleware
+    this.middleware();
 
+    // Load core app
+    this.loadCore();
 
-    bootstrap() {
-        // Connect to database.
-        this.database();
+    // Load auth app
+    this.loadAuth();
 
-        // Set up middleware
-        this.middleware();
+    // Load installed apps.
+    this.loadApps();
 
-        // Load core app
-        this.loadCore();
+    // Create server.
+    this.listen();
+  }
 
-        // Load auth app
-        this.loadAuth();
+  database() {
+    if (!this.config.database) return;
 
-        // Load installed apps.
-        this.loadApps();
+    this.log.debug("Server Setup: Connecting to database.");
+    mongoose.Promise = global.Promise;
+    var promise = mongoose.connect(this.config.database, {
+      useMongoClient: true
+    });
 
-        // Create server.
-        this.listen();
-    }
+    return promise.then(
+      db => {
+        db.on("error", err => this.log.error(err.message));
+        db.on("open", () => this.log.debug("MongoDB connection open"));
+        db.on("close", () => this.log.debug("MongoDB connection closed"));
+      },
+      err => {
+        this.log.error("Could not connect to MongoDB");
+      }
+    );
+  }
 
+  listen() {
+    this.log.debug("Server Setup: Creating server listener");
+    this.app.use(
+      errorhandler({
+        showStack: true,
+        dumpExceptions: true
+      })
+    );
 
-    database() {
-        
-        if (!this.config.database) return;
+    this.server = this.app.listen(this.config.port);
 
-        this.log.debug("Server Setup: Connecting to database.");
-        mongoose.Promise = global.Promise;
-        var promise = mongoose.connect(this.config.database, {
-            useMongoClient: true
-        });
+    this.log.debug("XMEN server starting on port " + this.config.port);
+  }
 
-        return promise.then((db) => {
-            db.on('error', (err) => this.log.error(err.message));
-            db.on('open', () => this.log.debug("MongoDB connection open"));
-            db.on('close', () => this.log.debug("MongoDB connection closed"));
-        }, (err) => {
-            this.log.error("Could not connect to MongoDB");
-        });
-    }
+  middleware() {
+    this.log.debug("Server Setup: Loading middleware");
+    this.app.set("showStackError", true);
 
+    this.app.use(helmet());
 
-    listen() {
-        this.log.debug("Server Setup: Creating server listener");
-        this.app.use(errorhandler({
-            showStack: true,
-            dumpExceptions: true
-        }));
+    // Compress static files.
+    this.app.use(
+      compression({
+        filter: function(req, res) {
+          return /json|text|javascript|css/.test(res.getHeader("Content-Type"));
+        },
+        level: 9
+      })
+    );
 
-        this.server = this.app.listen(this.config.port);
+    // Set the static root to serve files.
+    this.app.use(express.static(this.config.staticRoot));
 
-        this.log.debug("XMEN server starting on port " + this.config.port);
-    }
+    this.app.use(morgan("dev"));
 
+    // Set the templating engine.
+    this.app.set("view engine", "pug");
 
-    middleware() {
-        this.log.debug("Server Setup: Loading middleware");
-        this.app.set("showStackError", true);
+    // Set view cache
+    this.app.set("view cache", this.config.cacheView);
 
-        this.app.use(helmet());
+    // Enable jsonp
+    this.app.enable("jsonp callback");
 
-        // Compress static files.
-        this.app.use(
-            compression({
-                filter: function (req, res) {
-                    return /json|text|javascript|css/.test(res.getHeader("Content-Type"));
-                },
-                level: 9
-            })
-        );
+    // Connect body parser
+    this.app.use(bodyParser.json({}));
+    this.app.use(bodyParser.urlencoded({ extended: true }));
 
-        // Set the static root to serve files.
-        this.app.use(express.static(this.config.staticRoot));
+    // Use method override
+    this.app.use(methodOverride());
 
-        this.app.use(morgan("dev"));
+    // Use passport session
+    this.app.use(passport.initialize());
+    this.app.use(cookieParser());
+    this.app.use(passport.session());
 
-        // Set the templating engine.
-        this.app.set("view engine", "pug");
+    this.app.use(cors());
 
-        // Set view cache
-        this.app.set("view cache", this.config.cacheView);
+    // Set cors.
+    this.app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Credentials", true);
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE"
+      );
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+      );
+      next();
+    });
+  }
 
-        // Enable jsonp
-        this.app.enable("jsonp callback");
+  loadCore() {
+    // Load core models.
+    require("./core/models/content_type");
+  }
 
-        // Connect body parser
-        this.app.use(bodyParser.json({}));
-        this.app.use(bodyParser.urlencoded({ extended: true }));
+  loadAuth() {
+    // Load auth models.
+    require("./auth/models/permission");
+    require("./auth/models/group");
+    require("./auth/models/user");
+    require("./auth/models/token");
 
-        // Use method override
-        this.app.use(methodOverride());
+    const ContentType = mongoose.model("ContentType");
+    const Permission = mongoose.model("Permission");
+    const Group = mongoose.model("Group");
+    const User = mongoose.model("User");
+    const Token = mongoose.model("Token");
 
-        // Use passport session
-        this.app.use(passport.initialize());
-        this.app.use(cookieParser());
-        this.app.use(passport.session());
+    // Add content types to database.
+    ContentType.addModel(
+      "xmen_auth",
+      Permission.collection.name,
+      (err, model) => {
+        // Create permissions
+        Permission.createModelPermissions(model);
+      }
+    );
+    ContentType.addModel("xmen_auth", Group.collection.name, (err, model) => {
+      // Create permissions
+      Permission.createModelPermissions(model);
+    });
+    ContentType.addModel("xmen_auth", User.collection.name, (err, model) => {
+      // Create permissions
+      Permission.createModelPermissions(model);
+    });
+  }
 
-        this.app.use(cors());
-
-        // Set cors.
-        this.app.use((req, res, next) => {
-            res.header("Access-Control-Allow-Credentials", true);
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header(
-                "Access-Control-Allow-Methods",
-                "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE"
-            );
-            res.header(
-                "Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-            );
-            next();
-        });
-    }
-
-
-    loadCore() {
-        // Load core models.
-        require('./core/models/content_type');
-    }
-
-
-    loadAuth() {
-        // Load auth models.
-        require('./auth/models/permission');
-        require('./auth/models/group');
-        require('./auth/models/user');
-
-        const ContentType = mongoose.model('ContentType');
-        const Permission = mongoose.model('Permission');
-        const Group = mongoose.model('Group');
-        const User = mongoose.model('User');
-
-        // Add content types to database.
-        ContentType.addModel('xmen_auth', Permission.collection.name, (err, model) => {
-            // Create permissions
-            Permission.createModelPermissions(model);
-        });
-        ContentType.addModel('xmen_auth', Group.collection.name, (err, model) => {
-            // Create permissions
-            Permission.createModelPermissions(model);
-        });
-        ContentType.addModel('xmen_auth', User.collection.name, (err, model) => {
-            // Create permissions
-            Permission.createModelPermissions(model);
-        });
-    }
-
-
-    loadApps() {
-        this.log.debug("Server Setup: Registering apps");
-        AppRegistry.registerApps(this.config.installedApps);
-    }
+  loadApps() {
+    this.log.debug("Server Setup: Registering apps");
+    AppRegistry.registerApps(this.config.installedApps);
+  }
 }
 
 module.exports = new Xmen();
